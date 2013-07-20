82fc2d817ed2312e78f60b582768c6844004ecfb
Laszlo Kovacs <android1_1@hotmail.com>
2013-07-19 19:52:03 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpgFinal::Application.config.secret_token = '150054ae17424f9fa790c0137ce4677097bf99eb6ffeb0b30377f4239d5424927f9f4fcec829dbabf6ec94279f2180445df1f616e237bfb6ec0d5d944093f1a7'
